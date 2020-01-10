6acd719237ad48f7085f6614f999fa3d2d80c87c
vasillaba <vasillaba1@gmail.com>
2020-01-10 09:09:35 +0200

    start

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=windows-1251" />
+	<link href="css/style.css" rel="stylesheet" type="text/css" />
+    <link href="/css/reset.css" rel="stylesheet" type="text/css" />
+    
+	<title>NOBrand</title>
+</head>
+
+<body>
+
+    <?php
+        include("include/header.php");
+    ?>
+    
+    <section class="body">
+		<div class="container">
+			<h1>Перший український електромопед</h1>
+
+			<div class="moped">
+				<a href="orders.php">Перейти до покупки</a>
+			</div>
+		</div>
+	</section>
+    
+    
+    <?php
+        include("include/footer.php");
+    ?>
+    
+
+</body>
+</html>
